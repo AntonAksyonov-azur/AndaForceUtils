523cb6432d87275b7257630776cd4923a762f47e
arazect <za3a24@yandex.ru>
2014-04-15 12:07:55 +0700

    Initial commit, added Between extension methods

---
+++ AndaForceExtensions/com/andaforce/arazect/variables/IsValueBetween.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace AndaForceExtensions.com.andaforce.arazect.variables
+{
+    public static class IsValueBetween
+    {
+        /// <summary>
+        /// Проверяет попадание класса, реализующего интерефейс IComparable, в диапазон значений
+        /// Обе границы будут включены
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="min">Нижняя граница для сравнения</param>
+        /// <param name="max">Верхняя граница для сравнения</param>
+        /// <returns>
+        /// true, если значение value находится между min и max
+        /// false, если значнеие value находится вне диапазона
+        /// </returns>
+        public static bool BetweenII<T>(this T value, T min, T max) where T : IComparable
+        {
+            //(min.CompareTo(value) <= 0) && (value.CompareTo(max) <= 0);
+            return (value.CompareTo(min) >= 0) && (value.CompareTo(max) <= 0);
+        }
+
+        /// <summary>
+        /// Проверяет попадание класса, реализующего интерефейс IComparable, в диапазон значений
+        /// Обе границы будут исключены
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="min">Нижняя граница для сравнения</param>
+        /// <param name="max">Верхняя граница для сравнения</param>
+        /// <returns>
+        /// true, если значение value находится между min и max
+        /// false, если значнеие value находится вне диапазона
+        /// </returns>
+        public static bool BetweenEE<T>(this T value, T min, T max) where T : IComparable
+        {
+            return (value.CompareTo(min) > 0) && (value.CompareTo(max) < 0);
+        }
+
+        /// <summary>
+        /// Проверяет попадание класса, реализующего интерефейс IComparable, в диапазон значений
+        /// Нижняя граница будет включена, тогда как верхняя исключена из диапазона
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="min">Нижняя граница для сравнения</param>
+        /// <param name="max">Верхняя граница для сравнения</param>
+        /// <returns>
+        /// true, если значение value находится между min и max
+        /// false, если значнеие value находится вне диапазона
+        /// </returns>
+        public static bool BetweenIE<T>(this T value, T min, T max) where T : IComparable
+        {
+            return (value.CompareTo(min) >= 0) && (value.CompareTo(max) < 0);
+        }
+
+        /// <summary>
+        /// Проверяет попадание класса, реализующего интерефейс IComparable, в диапазон значений
+        /// Нижняя граница будет исключена, тогда как верхняя включена в диапазон
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="min">Нижняя граница для сравнения</param>
+        /// <param name="max">Верхняя граница для сравнения</param>
+        /// <returns>
+        /// true, если значение value находится между min и max
+        /// false, если значнеие value находится вне диапазона
+        /// </returns>
+        public static bool BetweenEI<T>(this T value, T min, T max) where T : IComparable
+        {
+            return (value.CompareTo(min) > 0 && value.CompareTo(max) <= 0);
+        }
+    }
+}
---
+++ AndaForceExtensionsTest/com/andaforce/arazect/variables/IsValueBetweenTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using AndaForceExtensions.com.andaforce.arazect.variables;
+using NUnit.Framework;
+
+namespace AndaForceExtensionsTest.com.andaforce.arazect.variables
+{
+    [TestFixture]
+    public class IsValueBetweenTest
+    {
+        private static readonly object[] DataII =
+        {
+            new TestData {Value = 5, Min = 0, Max = 9, ExpectedResult = true},
+            new TestData {Value = 5, Min = 5, Max = 6, ExpectedResult = true},
+            new TestData {Value = 5, Min = 4, Max = 5, ExpectedResult = true},
+            new TestData {Value = 5, Min = 5, Max = 5, ExpectedResult = true},
+            new TestData {Value = "A", Min = "A", Max = "B", ExpectedResult = true},
+            new TestData {Value = "B", Min = "A", Max = "C", ExpectedResult = true},
+            new TestData {Value = "B", Min = "B", Max = "B", ExpectedResult = true}
+        };
+
+        private static readonly object[] DataEE =
+        {
+            new TestData {Value = 5, Min = 0, Max = 9, ExpectedResult = true},
+            new TestData {Value = 5, Min = 5, Max = 6, ExpectedResult = false},
+            new TestData {Value = 5, Min = 4, Max = 5, ExpectedResult = false},
+            new TestData {Value = 5, Min = 5, Max = 5, ExpectedResult = false},
+            new TestData {Value = "A", Min = "A", Max = "B", ExpectedResult = false},
+            new TestData {Value = "B", Min = "A", Max = "C", ExpectedResult = true},
+            new TestData {Value = "B", Min = "B", Max = "B", ExpectedResult = false}
+        };
+
+        private static readonly object[] DataIE =
+        {
+            new TestData {Min = 0, Value = 5, Max = 9, ExpectedResult = true},
+            new TestData {Min = 5, Value = 5, Max = 6, ExpectedResult = true},
+            new TestData {Min = 4, Value = 5, Max = 5, ExpectedResult = false},
+            new TestData {Min = 5, Value = 5, Max = 5, ExpectedResult = false},
+            new TestData {Min = 6, Value = 5, Max = 5, ExpectedResult = false},
+            new TestData {Value = "A", Min = "A", Max = "B", ExpectedResult = true},
+            new TestData {Value = "B", Min = "A", Max = "C", ExpectedResult = true},
+            new TestData {Value = "B", Min = "B", Max = "B", ExpectedResult = false}
+        };
+
+        private static readonly object[] DataEI =
+        {
+            new TestData {Min = 0, Value = 5, Max = 9, ExpectedResult = true},
+            new TestData {Min = 5, Value = 5, Max = 6, ExpectedResult = false},
+            new TestData {Min = 4, Value = 5, Max = 5, ExpectedResult = true},
+            new TestData {Min = 5, Value = 5, Max = 5, ExpectedResult = false},
+            new TestData {Min = 6, Value = 5, Max = 5, ExpectedResult = false},
+            new TestData {Value = "A", Min = "A", Max = "B", ExpectedResult = false},
+            new TestData {Value = "B", Min = "A", Max = "C", ExpectedResult = true},
+            new TestData {Value = "B", Min = "B", Max = "B", ExpectedResult = false}
+        };
+
+        private string ErrorMessage(TestData v)
+        {
+            return String.Format("Failed with arguments  min={0}, v={1}, max={2}", v.Min, v.Value, v.Max);
+        }
+
+        [Test, TestCaseSource("DataII")]
+        public void TestII(TestData v)
+        {
+            Assert.AreEqual(v.ExpectedResult, v.Value.BetweenII(v.Min, v.Max), ErrorMessage(v));
+        }
+
+        [Test, TestCaseSource("DataEE")]
+        public void TestEE(TestData v)
+        {
+            Assert.AreEqual(v.ExpectedResult, v.Value.BetweenEE(v.Min, v.Max), ErrorMessage(v));
+        }
+
+        [Test, TestCaseSource("DataEI")]
+        public void TestEI(TestData v)
+        {
+            Assert.AreEqual(v.ExpectedResult, v.Value.BetweenEI(v.Min, v.Max), ErrorMessage(v));
+        }
+
+        [Test, TestCaseSource("DataIE")]
+        public void TestIE(TestData v)
+        {
+            Assert.AreEqual(v.ExpectedResult, v.Value.BetweenIE(v.Min, v.Max), ErrorMessage(v));
+        }
+
+        public class TestData
+        {
+            public IComparable Value;
+            public IComparable Min;
+            public IComparable Max;
+            public bool ExpectedResult;
+        }
+    }
+}
