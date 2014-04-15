a8ac3c8b58e7bc7361787656ae17fbca53ce6c61
arazect <za3a24@yandex.ru>
2014-04-15 13:57:27 +0700

    Added EnumExtension

---
+++ AndaForceExtensions/com/andaforce/arazect/enumerations/EnumExtension.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AndaForceExtensions.com.andaforce.arazect.enumerations
+{
+    public static class EnumExtension
+    {
+        public static String GetStringValue(this Enum value)
+        {
+            string output = null;
+            var attrs = value
+                .GetType()
+                .GetField(value.ToString())
+                .GetCustomAttributes(typeof (StringValue), false) as StringValue[];
+
+            if (attrs != null && attrs.Length > 0)
+            {
+                output = attrs[0].Value;
+            }
+
+            return output;
+        }
+
+        public class StringValue : Attribute
+        {
+            private readonly string _value;
+
+            public StringValue(string value)
+            {
+                _value = value;
+            }
+
+            public string Value
+            {
+                get { return _value; }
+            }
+        }
+    }
+}
