e0d1c7e3966985e55d7ed21e0780351bb34bc98e
arazect <za3a24@yandex.ru>
2014-04-15 12:36:03 +0700

    Added ConfigurationLoader

---
+++ AndaForceExtensions/com/andaforce/arazect/serialization/ConfigurationLoader.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace AndaForceExtensions.com.andaforce.arazect.serialization
+{
+    public class ConfigurationLoader
+    {
+        #region Load
+
+        public static T LoadConfiguration<T>(
+            String loadPath,
+            InfoDelegate onError = null,
+            InfoDelegate onSuccess = null)
+            where T : IConfigurationObject, new()
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof (T));
+                TextReader reader = new StreamReader(loadPath);
+                T instance = (T) serializer.Deserialize(reader);
+                reader.Close();
+
+                ReportSuccess<T>("Deserialization", loadPath, onSuccess);
+
+                return instance;
+            }
+            catch (Exception e)
+            {
+                if (onError != null)
+                {
+                    ReportError<T>("Deserialization", onError, e);
+                }
+
+                T instance = new T();
+                instance.InitDefault();
+                return instance;
+            }
+        }
+
+
+        public static T LoadConfigurationFromString<T>(String source,
+            InfoDelegate onError = null,
+            InfoDelegate onSuccess = null) where T : IConfigurationObject, new()
+        {
+            try
+            {
+                var serializer = new XmlSerializer(typeof (T));
+                TextReader reader = new StringReader(source);
+                T instance = (T) serializer.Deserialize(reader);
+                reader.Close();
+
+                ReportSuccess<T>("Deserialization", "Raw source", onSuccess);
+
+                return instance;
+            }
+            catch (Exception e)
+            {
+                ReportError<T>("Deserialization", onError, e);
+
+                T instance = new T();
+                instance.InitDefault();
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Save
+
+        public static void SaveConfiguration<T>(
+            T configurationObject,
+            String savePath,
+            InfoDelegate onSuccess = null,
+            InfoDelegate onError = null) where T : IConfigurationObject, new()
+        {
+            try
+            {
+                String dirPath = Path.GetDirectoryName(savePath);
+                if (dirPath != null && !Directory.Exists(dirPath))
+                {
+                    Directory.CreateDirectory(dirPath);
+                }
+
+                var serializer = new XmlSerializer(typeof (T));
+                TextWriter writer = new StreamWriter(savePath);
+                serializer.Serialize(writer, configurationObject);
+                writer.Close();
+
+                ReportSuccess<T>("Serialization", savePath, onSuccess);
+            }
+            catch (Exception e)
+            {
+                ReportError<T>("Serialization", onError, e);
+            }
+        }
+
+        #endregion
+
+        private static void ReportError<T>(String typeMessage, InfoDelegate onError, Exception e)
+            where T : new()
+        {
+            onError.Invoke(
+                String.Format("{0} error: {1}", typeMessage, e.Message));
+        }
+
+        private static void ReportSuccess<T>(String typeMessage, string path, InfoDelegate onSuccess)
+            where T : new()
+        {
+            if (onSuccess != null)
+            {
+                onSuccess.Invoke(
+                    String.Format("{0} completed with {1} at {2}", typeMessage, typeof (T), path));
+            }
+        }
+    }
+
+    public interface IConfigurationObject
+    {
+        void InitDefault();
+    }
+
+    public delegate void InfoDelegate(String message);
+}
