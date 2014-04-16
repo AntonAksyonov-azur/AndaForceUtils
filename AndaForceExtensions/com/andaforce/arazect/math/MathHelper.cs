61d05072ebe255c0f6a8dee373447067566ef905
arazect <za3a24@yandex.ru>
2014-04-16 11:11:10 +0700

    Added MathHelper and Clamp functions

---
+++ AndaForceExtensions/com/andaforce/arazect/math/MathHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace AndaForceExtensions.com.andaforce.arazect.math
+{
+    public static class MathHelper
+    {
+        public static float Clamp(float value, float min, float max)
+        {
+            if (value > max) return max;
+            if (value < min) return min;
+            return value;
+        }
+
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value > max) return max;
+            if (value < min) return min;
+            return value;
+        }
+    }
+}
---
+++ AndaForceExtensionsTest/com/andaforce/arazect/math/MathHelperTests.cs
@@ -0,0 +1,55 @@
+﻿using AndaForceExtensions.com.andaforce.arazect.math;
+using NUnit.Framework;
+
+namespace AndaForceExtensionsTest.com.andaforce.arazect.math
+{
+    [TestFixture]
+    public class MathHelperTests
+    {
+        private static readonly object[] TestCaseSourceFloat =
+        {
+            new TestDataFloat() {A = 5.0f, Min = 0.0f, Max = 10.0f, Expected = 5.0f},
+            new TestDataFloat() {A = 10.0f, Min = 0.0f, Max = 10.0f, Expected = 10.0f},
+            new TestDataFloat() {A = 0.0f, Min = 0.0f, Max = 10.0f, Expected = 0.0f},
+            new TestDataFloat() {A = 99.0f, Min = 0.0f, Max = 11.0f, Expected = 11.0f},
+            new TestDataFloat() {A = -99.0f, Min = 0.0f, Max = 11.0f, Expected = 0.0f},
+        };
+
+        private static readonly object[] TestCaseSourceInt =
+        {
+            new TestDataInt() {A = 5, Min = 0, Max = 10, Expected = 5},
+            new TestDataInt() {A = 10, Min = 0, Max = 10, Expected = 10},
+            new TestDataInt() {A = 0, Min = 0, Max = 10, Expected = 0},
+            new TestDataInt() {A = 99, Min = 0, Max = 11, Expected = 11},
+            new TestDataInt() {A = -99, Min = 0, Max = 11, Expected = 0},
+        };
+
+        [Test, TestCaseSource("TestCaseSourceFloat")]
+        public void TestClampFloat(TestDataFloat v)
+        {
+            Assert.AreEqual(v.Expected, MathHelper.Clamp(v.A, v.Min, v.Max));
+        }
+
+        [Test, TestCaseSource("TestCaseSourceInt")]
+        public void TestClampInt(TestDataInt v)
+        {
+            Assert.AreEqual(v.Expected, MathHelper.Clamp(v.A, v.Min, v.Max));
+        }
+
+        public class TestDataFloat
+        {
+            public float A;
+            public float Min;
+            public float Max;
+            public float Expected;
+        }
+
+        public class TestDataInt
+        {
+            public int A;
+            public int Min;
+            public int Max;
+            public int Expected;
+        }
+    }
+}
