2242d11ed1366f25a1925511fe96b61bb3978000
arazect <za3a24@yandex.ru>
2014-05-12 15:53:44 +0700

    Added wrapper for Console

---
+++ AndaForceExtensions/com/andaforce/arazect/console/ConsoleWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AndaForceExtensions.com.andaforce.arazect.console
+{
+    public static class ConsoleWrapper
+    {
+        public static void WriteUserWaitPattern()
+        {
+            Console.WriteLine("\n\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
