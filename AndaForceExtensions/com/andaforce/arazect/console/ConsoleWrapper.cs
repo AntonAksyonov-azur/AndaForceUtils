b2ea117c67fbbeb40ba92907aafaedca488fd296
arazect <za3a24@yandex.ru>
2014-05-12 16:37:19 +0700

    Function renamed to WriteAnyKeyPrompt

--- AndaForceExtensions/com/andaforce/arazect/console/ConsoleWrapper.cs
+++ AndaForceExtensions/com/andaforce/arazect/console/ConsoleWrapper.cs
@@ -4,7 +4,7 @@ namespace AndaForceExtensions.com.andaforce.arazect.console
 {
     public static class ConsoleWrapper
     {
-        public static void WriteUserWaitPattern()
+        public static void WriteAnyKeyPrompt()
         {
             Console.WriteLine("\n\nPress any key to continue...");
             Console.ReadKey();
