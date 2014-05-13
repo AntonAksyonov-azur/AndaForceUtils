6fcffb5af5ed5cf9103227070768b0946a8db6e9
arazect <za3a24@yandex.ru>
2014-05-13 13:49:26 +0700

    Added Test for LinearArray and updated its code

--- AndaForceExtensions/com/andaforce/arazect/collections/generic/LinearArray.cs
+++ AndaForceExtensions/com/andaforce/arazect/collections/generic/LinearArray.cs
@@ -19,28 +19,35 @@ namespace AndaForceExtensions.com.andaforce.arazect.collections.generic
             ArraySource = new T[width * height];
         }
 
+        /// <summary>
+        /// Converts Two-dimensional array to one-dimensional. First index assotiated with x, second with y
+        /// </summary>
+        /// <param name="arraySource"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
         public static LinearArray<T> FromTwoDimensional(T[,] arraySource, int width, int height)
         {
             var result = new LinearArray<T>(width, height);
-            for (int i = 0; i < height; i++)
+            for (int y = 0; y < height; y++)
             {
-                for (int j = 0; j < width; j++)
+                for (int x = 0; x < width; x++)
                 {
-                    result.Set(arraySource[i, j], j, i);
+                    result.SetItem(arraySource[x, y], x, y);
                 }
             }
 
             return result;
         }
 
-        public T Get(int x, int y)
+        public T GetItem(int x, int y)
         {
             CheckBorders(x, y);
 
             return ArraySource[x + y * Height];
         }
 
-        public void Set(T item, int x, int y)
+        public void SetItem(T item, int x, int y)
         {
             CheckBorders(x, y);
 
@@ -54,7 +61,7 @@ namespace AndaForceExtensions.com.andaforce.arazect.collections.generic
             {
                 for (int j = 0; j < Width; j++)
                 {
-                    result[i, j] = Get(i, j);
+                    result[i, j] = GetItem(i, j);
                 }
             }
 
---
+++ AndaForceExtensionsTest/com/andaforce/arazect/collections/generic/LinearArrayTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using AndaForceExtensions.com.andaforce.arazect.collections.generic;
+using AndaForceExtensions.com.andaforce.arazect.collections.generic.extension;
+using NUnit.Framework;
+
+namespace AndaForceExtensionsTest.com.andaforce.arazect.collections.generic
+{
+    [TestFixture]
+    public class LinearArrayTest
+    {
+        private Random _rnd;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _rnd = new Random();
+        }
+
+        [Test]
+        public void TestBasicMethods()
+        {
+            const int dimension = 100;
+            var linearArray = new LinearArray<int>(dimension, dimension);
+
+            for (int i = 0; i < dimension; i++)
+            {
+                for (int j = 0; j < dimension; j++)
+                {
+                    var rndValue = _rnd.Next();
+                    linearArray.SetItem(rndValue, i, j);
+                    Assert.AreEqual(rndValue, linearArray.GetItem(i, j), "Wrong item received");
+                }
+            }
+        }
+
+        [Test]
+        public void TestConversionToLinear()
+        {
+            const int dimension = 100;
+            var twoDimArray = new int[dimension, dimension];
+            for (int y = 0; y < dimension; y++)
+            {
+                for (int x = 0; x < dimension; x++)
+                {
+                    twoDimArray[x, y] = _rnd.Next();
+                }
+            }
+
+            var linearArray = LinearArray<int>.FromTwoDimensional(twoDimArray, dimension, dimension);
+            for (int y = 0; y < dimension; y++)
+            {
+                for (int x = 0; x < dimension; x++)
+                {
+                    Assert.AreEqual(twoDimArray[x, y], linearArray.GetItem(x, y), "Wrong item received");
+                }
+            }
+        }
+
+        [Test]
+        public void TestConversionToTwoDimensional()
+        {
+            const int dimension = 100;
+            var linearArray = new LinearArray<int>(dimension, dimension);
+            for (int y = 0; y < dimension; y++)
+            {
+                for (int x = 0; x < dimension; x++)
+                {
+                    linearArray.SetItem(_rnd.Next(), x, y);
+                }
+            }
+
+            var twoDimensional = linearArray.ToTwoDimensional();
+            for (int y = 0; y < dimension; y++)
+            {
+                for (int x = 0; x < dimension; x++)
+                {
+                    Assert.AreEqual(linearArray.GetItem(x, y), twoDimensional[x, y], "Wrong item received");
+                }
+            }
+        }
+
+        [Test, ExpectedException]
+        public void TestCausesException()
+        {
+            var linear = new LinearArray<int>(3, 3);
+            linear.SetItem(999, 999, 999);
+        }
+    }
+}
