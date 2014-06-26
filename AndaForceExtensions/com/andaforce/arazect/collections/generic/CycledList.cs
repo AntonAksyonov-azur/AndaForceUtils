7045676fa2390d146664207c733fabee914053d5
arazect <za3a24@yandex.ru>
2014-06-26 14:05:49 +0700

    Added CycledList

---
+++ AndaForceExtensions/com/andaforce/arazect/collections/generic/CycledList.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AndaForceExtensions.com.andaforce.arazect.collections.generic
+{
+    public class CycledList<T>
+    {
+        public List<T> ListSource { get; private set; }
+        public int CurrentPosition { get; private set; }
+
+        public int Lenght
+        {
+            get { return ListSource.Count; }
+        }
+
+        #region Constructors
+
+        public CycledList()
+        {
+            ListSource = new List<T>();
+            CurrentPosition = -1;
+        }
+
+        public CycledList(IEnumerable<T> source)
+        {
+            ListSource = new List<T>(source);
+            CurrentPosition = -1;
+        }
+
+        #endregion
+
+        #region Public methods
+
+        public void Add(T value)
+        {
+            ListSource.Add(value);
+        }
+
+        public T GetCurrent()
+        {
+            if (!ListSource.Any())
+            {
+                throw new IndexOutOfRangeException("No elements in CycledList presented");
+            }
+
+            if (CurrentPosition == -1)
+            {
+                CurrentPosition = 0;
+            }
+
+            return ListSource[CurrentPosition];
+        }
+
+        public T GetNext()
+        {
+            SwitchPosition(1);
+            return GetCurrent();
+        }
+
+        public T GetPrev()
+        {
+            SwitchPosition(-1);
+            return GetCurrent();
+        }
+
+        public void SwithPositionToEnd()
+        {
+            CurrentPosition = ListSource.Count - 1;
+        }
+
+        public void SwitchPositionToStart()
+        {
+            CurrentPosition = 0;
+        }
+
+        #endregion
+
+        private void SwitchPosition(int direction)
+        {
+            CurrentPosition += direction;
+            if (CurrentPosition < 0)
+            {
+                CurrentPosition = ListSource.Count - 1;
+            }
+            else
+            {
+                CurrentPosition = CurrentPosition % ListSource.Count;    
+            }
+        }
+    }
+}
