cc372c67968bc0a7e6568e50c29dcf37dc334830
arazect <za3a24@yandex.ru>
2014-07-10 15:15:13 +0700

    Changed project version

--- AndaForceExtensions/Properties/AssemblyInfo.cs
+++ AndaForceExtensions/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("AndaForce Extensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AndaForceUtils")]
+[assembly: AssemblyDescription("AndaForce Utility Classes Collection")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("AndaForce")]
-[assembly: AssemblyProduct("AndaForce Extensions")]
+[assembly: AssemblyProduct("AndaForce Utils")]
 [assembly: AssemblyCopyright("Copyright © AndaForce corp.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
